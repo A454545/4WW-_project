4dab022146d631f34eabfd4b42a0c3beb57d559d
alyasira <alyasira@mcmaster.ca>
2021-11-12 19:39:34 -0500

    performance

--- assets/js/individualResult.js
+++ assets/js/individualResult.js
@@ -17,31 +17,31 @@ function generateMap() {
 	L.marker([latitude, longitude]).addTo(myMap);
 	/* Image gallery code*/
 
-	var slideIndex = 1;
+	window.slideIndex = 1;
 	showSlides(slideIndex);
+}
 
-	// Next/previous controls
-	function plusSlides(n) {
-		showSlides(slideIndex += n);
-	}
-	// Thumbnail image controls
-	function currentSlide(n) {
-		showSlides(slideIndex = n);
-	}
+// Next/previous controls
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
 
-	// Image gallery function (cycles through slides)
-	function showSlides(n) {
-		var i;
-		var slides = document.getElementsByClassName("mySlides");
-		if (n > slides.length) {
-			slideIndex = 1;
-		}
-		if (n < 1) {
-			slideIndex = slides.length;
-		}
-		for (i = 0; i < slides.length; i++) {
-			slides[i].style.display = "none";
-		}
-		slides[slideIndex - 1].style.display = "flex";
+// Image gallery function (cycles through slides)
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	if (n > slides.length) {
+		slideIndex = 1;
 	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "flex";
 }
