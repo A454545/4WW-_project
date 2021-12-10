40877eaf06505c7fd37360283c684169517a0779
alyasira <alyasira@mcmaster.ca>
2021-12-09 21:36:01 -0500

    final tables

--- tables.sql
+++ tables.sql
@@ -10,33 +10,31 @@ UNIQUE  `email` (`email`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE `trueroofs`.`listing` ( 
-`id` INT(10) NOT NULL AUTO_INCREMENT , 
-`name` VARCHAR(50) NOT NULL , 
-`description` TEXT NOT NULL , 
-`address` VARCHAR(100) NOT NULL , 
-`dlat` VARCHAR(15) NOT NULL , 
-`dlong` VARCHAR(15) NOT NULL , 
-`source` VARCHAR(200) NOT NULL , 
-`price` INT(20) NOT NULL , 
-`image` BLOB NULL , 
-PRIMARY KEY (`id`)
+`id` INT(10) NOT NULL AUTO_INCREMENT ,  
+`name` VARCHAR(50) NOT NULL ,  
+`description` TEXT NOT NULL ,  
+`address` VARCHAR(250) NULL DEFAULT NULL ,  
+`dlat` VARCHAR(15) NOT NULL ,  
+`dlong` VARCHAR(15) NOT NULL ,  
+`source` VARCHAR(250) NOT NULL ,  
+`price` FLOAT NOT NULL ,  
+`imagePath` VARCHAR(250) NOT NULL ,  
+`imageURL` VARCHAR(250) NOT NULL ,    
+PRIMARY KEY  (`id`)
 ) ENGINE = InnoDB;
-CREATE TABLE `trueroofs`.`listing` ( `id` INT(10) NOT NULL AUTO_INCREMENT ,  `name` VARCHAR(50) NOT NULL ,  `description` TEXT NOT NULL ,  `address` VARCHAR(100) NOT NULL ,  `dlat` VARCHAR(15) NOT NULL ,  `dlong` VARCHAR(15) NOT NULL ,  `source` VARCHAR(200) NOT NULL ,  `price` INT(20) NOT NULL ,  `image` BLOB NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;
 
-CREATE TABLE `trueroofs`.`review` (
-`id` int(15) NOT NULL AUTO_INCREMENT,
-`reviewerID` int(5) NOT NULL,
-`locationID` int(10) NOT NULL,
-`rating` float NOT NULL,
+CREATE TABLE `trueroofs`.`review` ( 
+`id` INT(15) NOT NULL AUTO_INCREMENT ,  
+`name` VARCHAR(50) NOT NULL ,  
+`reviewerID` INT(5) NOT NULL ,  
+`reviewerName` VARCHAR(50) NOT NULL ,  
+`locationID` VARCHAR(10) NULL DEFAULT NULL ,  
+`dlat` VARCHAR(15) NOT NULL ,  
+`dlong` VARCHAR(15) NOT NULL ,  
+`content` TEXT NOT NULL ,  
+`rating` FLOAT NOT NULL ,  
 `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,  
-`content` text NOT NULL,
-`dlat` VARCHAR(15) NOT NULL , 
-`dlong` VARCHAR(15) NOT NULL , 
-`image` BLOB NULL ,
-`video` BLOB NULL ,
-PRIMARY KEY (`id`),
-KEY `location` (`locationID`),
-KEY `reviewer` (`reviewerID`),
-CONSTRAINT `location` FOREIGN KEY (`locationID`) REFERENCES `listing` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-CONSTRAINT `reviewer` FOREIGN KEY (`reviewerID`) REFERENCES `user` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB
+`imagePath` VARCHAR(250) NOT NULL ,    
+PRIMARY KEY  (`id`)
+) ENGINE = InnoDB;
+
